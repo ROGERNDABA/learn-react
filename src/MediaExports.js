7dbaf8980637196f08929619489b599c5c18ec3d
Rogerndaba <rogerndaba@gmail.com>
2019-03-08 16:41:01 +0200

    Tech stack images

---
+++ src/MediaExports.js
@@ -0,0 +1,45 @@
+import NodejsIMG from "./images/nodejs.png";
+import JSIMG from "./images/JavaScript.png";
+import CIMG from "./images/c.png";
+import CppIMG from "./images/cpp.png";
+import PythonIMG from "./images/python.png";
+import JavaIMG from "./images/java.png";
+import TSIMG from "./images/TS.png";
+import JqueryIMG from "./images/jquery.png";
+import BasicsIMG from "./images/basics.png";
+import FlaskIMG from "./images/flask.png";
+import PHPIMG from "./images/php.png";
+import AngularIMG from "./images/angular.svg";
+import ReactIMG from "./images/react.png";
+import BoostrapIMG from "./images/bootstrap.png";
+import CFIMG from "./images/cf.png";
+import Electron from "./images/electron.png";
+import SQLitelIMG from "./images/sqlite.png";
+import SQLIMG from "./images/sql.png";
+import MysqlIMG from "./images/mysql.png";
+import MongoIMG from "./images/mongo.png";
+import Ionic from "./images/ionic.png";
+
+export {
+    NodejsIMG,
+    JSIMG,
+    CIMG,
+    CppIMG,
+    PythonIMG,
+    JavaIMG,
+    TSIMG,
+    JqueryIMG,
+    BasicsIMG,
+    FlaskIMG,
+    PHPIMG,
+    BoostrapIMG,
+    AngularIMG,
+    ReactIMG,
+    CFIMG,
+    Electron,
+    SQLitelIMG,
+    SQLIMG,
+    MysqlIMG,
+    MongoIMG,
+    Ionic
+};
--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -1,43 +1,195 @@
 import React, { Component } from "react";
 import "./TechStack.css";
-import NodejsIMG from "../../images/nodejs.png";
-import JSIMG from "../../images/JavaScript.png";
+import {
+    JSIMG,
+    NodejsIMG,
+    CIMG,
+    CppIMG,
+    PythonIMG,
+    JavaIMG,
+    TSIMG,
+    JqueryIMG,
+    BasicsIMG,
+    FlaskIMG,
+    PHPIMG,
+    BoostrapIMG,
+    AngularIMG,
+    ReactIMG,
+    CFIMG,
+    SQLitelIMG,
+    SQLIMG,
+    MysqlIMG,
+    MongoIMG,
+    Ionic,
+    Electron
+} from "../../MediaExports";
 
 class TechStack extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="TechStackContainer">
-        <div className="Heading">Technology Stack</div>
+    state = {};
+    render() {
+        return (
+            <div className="TechStackContainer">
+                <div className="Heading">Technology Stack</div>
 
-        <div className="TechStackLogos">
-          <div className="container">
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
+                <div className="TechStackLogos">
+                    <div className="container">
+                        {/* ///////////// FUnDAMENTALS ///////// */}
+                        <h3>Fundamental Programming</h3>
+                        <div className="logos">
+                            <img src={CIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">C</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={CppIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">CPlusPlus</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={PythonIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Python</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={JavaIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+
+                        {/* ///////////// Web: Frontend ///////// */}
+                        <h3>Web Development: Front End and Basics</h3>
+                        <div className="logos">
+                            <img src={BasicsIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={JSIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={TSIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={JqueryIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={BoostrapIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+
+                        {/* ///////////// Web Backend ///////// */}
+                        <h3>
+                            Web Development: Back End, Frameworks and Libraries
+                        </h3>
+                        <div className="logos">
+                            <img src={NodejsIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={FlaskIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={PHPIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={AngularIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={ReactIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={Electron} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={CFIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+
+                        {/* ///////////// Databases ///////// */}
+                        <h3>Databases</h3>
+                        <div className="logos">
+                            <img src={SQLIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={MysqlIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img
+                                src={SQLitelIMG}
+                                style={{ width: "70%" }}
+                                alt=""
+                            />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={MongoIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+
+                        {/* ///////////// Mobile DEv ///////// */}
+                        <h3>Mobile Development</h3>
+                        <div className="logos">
+                            <img src={Ionic} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                        <div className="logos">
+                            <img src={ReactIMG} alt="" />
+                            <a href="/" className="stackName">
+                                <h1 className="logoName">Java</h1>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="logos">
-              <img src={JSIMG} alt="" />
-            </div>
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
-            </div>
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
-            </div>
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
-            </div>
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
-            </div>
-            <div className="logos">
-              <img src={NodejsIMG} alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default TechStack;
