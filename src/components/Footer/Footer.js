a99fdc5e5a1597561abbea356ec0dd91b0fca67d
Rogerndaba <rogerndaba@gmail.com>
2019-03-14 15:13:16 +0200

    touchups

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -64,7 +64,26 @@ class Footer extends Component {
             fontWeight: "400px"
           }}
         >
-          Copyright © 2019 Roger Ndaba
+          Copyright © 2019 Roger Ndaba <br />
+          <div>
+            Icons made by{" "}
+            <a href="https://www.flaticon.com/authors/vectors-market" rel="noopener noreferrer" title="Vectors Market">
+              Vectors Market
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.com/" title="Flaticon" rel="noopener noreferrer">
+              www.flaticon.com
+            </a>{" "}
+            is licensed by{" "}
+            <a
+              href="http://creativecommons.org/licenses/by/3.0/"
+              title="Creative Commons BY 3.0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CC 3.0 BY
+            </a>
+          </div>
         </div>
       </div>
     );
