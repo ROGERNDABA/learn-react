7c84459ab18e3fca914832df3246bcebc2631247
rogerndaba <rogerndaba@gmail.com>
2019-03-14 06:51:14 +0200

    Added projects path

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,42 +3,42 @@ import "./Home.css";
 import Typewriter from "../plugins/Typewriter";
 
 class Home extends Component {
-    // constructor(props) {
-    //   super(props);
-    // }
+  // constructor(props) {
+  //   super(props);
+  // }
 
-    componentDidMount() {
-        document.title = "Home";
-    }
+  componentDidMount() {
+    document.title = "Roger's Resume";
+  }
 
-    componentWillUnmount() {
-        clearTimeout(this.interval);
-    }
+  componentWillUnmount() {
+    clearTimeout(this.interval);
+  }
 
-    render() {
-        return (
-            <div className="homeContainer">
-                <div className="ImageContainer">
-                    <a href="/roger.png" target="_blank">
-                        <img
-                            className="profileImage"
-                            src="/roger.png"
-                            alt="The handsome and sexy roger"
-                            title="The handsome and sexy roger"
-                        />
-                    </a>
-                </div>
-                <div className="Quote">
-                    <em>
-                        <Typewriter
-                            text='"You have not experienced real programming until you make a 5 dimensional array, with objects at every even dimension and construct a logical function to work with it in one go, but then google how to change a string into a number" - Me'
-                            speed="50"
-                        />
-                    </em>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="homeContainer">
+        <div className="ImageContainer">
+          <a href="/roger.png" target="_blank">
+            <img
+              className="profileImage"
+              src="/roger.png"
+              alt="The handsome and sexy roger"
+              title="The handsome and sexy roger"
+            />
+          </a>
+        </div>
+        <div className="Quote">
+          <em>
+            <Typewriter
+              text='"You have not experienced real programming until you make a 5 dimensional array, with objects at every even dimension and construct a logical function to work with it in one go, but then google how to change a string into a number" - Me'
+              speed="50"
+            />
+          </em>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,44 +1,76 @@
 import React, { Component } from "react";
 import "./Projects.css";
 
+class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+  }
+  toggleAccordion = () => {
+    console.log(this.state.active);
+    this.setState(state => ({ active: !state.active }));
+  };
+
+  render() {
+    return (
+      <div style={{ padding: "10px" }}>
+        <button className="ProjectsAccordion" onClick={this.toggleAccordion}>
+          Section 2
+          <span style={{ float: "right", fontWeight: "bold" }}>
+            {!this.state.active ? "+" : "-"}
+          </span>
+        </button>
+        <div
+          className={"AccordionPanel " + (this.state.active ? "Stretched" : "")}
+        >
+          <p>
+            {this.props.ProjectDesc}
+            <p style={{ width: "100%" }}>
+              Project Repo: <a href="">asdasas</a>
+            </p>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-
-    let textInput = React.createRef();
   }
   render() {
     return (
       <div className="projectsContainer">
         <h1>Projects</h1>
-
-        <button className="ProjectsAccordion">Section 1</button>
-        <div className="AccordionPanel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
-          </p>
-        </div>
-
-        <button className="ProjectsAccordion">Section 2</button>
-        <div className="AccordionPanel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
-          </p>
-        </div>
-
-        <button className="ProjectsAccordion">Section 3</button>
-        <div className="AccordionPanel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
-          </p>
-        </div>
+        <span style={{ color: "rgba(255, 255, 255, 0.753)", padding: "10px" }}>
+          *These are all the projects I've done so far, and proud of, with their
+          corresponding Github links *
+        </span>
+        <Project
+          ProjectDesc="
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat."
+        />
+        <Project
+          ProjectDesc="
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat."
+        />
+        <Project
+          ProjectDesc="
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat."
+        />
       </div>
     );
   }
